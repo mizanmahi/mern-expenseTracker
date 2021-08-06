4d8ddd06e6ba7af7857da6fe9a6c9f4b455ed0e6
mizanmahi <mizanmahi24@gmail.com>
2021-08-07 03:00:01 +0600

    add: global context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/Incomeexpense';
+import TransactionList from './components/TransactionList';
+import AddTransactionForm from './components/AddTransactionForm';
+
+import { GlobalContextProvider } from './context/GlobalContext';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <div className='container'>
+        <GlobalContextProvider>
+          <Balance />
+          <IncomeExpense />
+          <TransactionList />
+          <AddTransactionForm />
+        </GlobalContextProvider>
+      </div>
     </div>
   );
 }
